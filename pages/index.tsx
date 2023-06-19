0e870b83742ffd1f3705245aa5e5149523fca08a
tj <thinupavani@gmail.com>
2023-06-19 13:17:54 +0530

    update buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,14 +23,13 @@ import Script from 'next/script';
 
 export default function Home() {
   const router = useRouter();
-  const [botAgentName, setBotAgentName] = useState('DFCC GPT')
+  const [botAgentName, setBotAgentName] = useState('DFCC GPT');
 
   const handleButtonClick = () => {
     router.push({
       pathname: '/chatbot',
     });
   };
-
 
   return (
     <>
@@ -40,55 +39,86 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossOrigin="anonymous"
+        ></link>
       </Head>
       <Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></Script>
-        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></Script>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossOrigin="anonymous"
+      ></Script>
 
-      <main className={`${styles.mainContainer} container-fluid m-0 m-lg-2 p-0`}>
-      <div className={`${styles.chatTopBar} d-flex flex-row `}>
-        <div className="col-12 text-center d-flex flex-row justify-content-between px-2">
-        <Image
-                src="/logo.jpg"
-                alt="AI"
-                width={150}
-                height={30}
-              />
+      <main
+        className={`${styles.mainContainer} container-fluid m-0 m-lg-2 p-0`}
+      >
+        <div className={`${styles.chatTopBar} d-flex flex-row `}>
+          <div className="col-12 text-center d-flex flex-row justify-content-between px-2">
+            <Image src="/logo.jpg" alt="AI" width={150} height={30} />
+          </div>
         </div>
-      </div>
-        <div className={`${styles.chatContainer}  d-flex flex-column align-items-center text-center`}>
+        <div
+          className={`${styles.chatContainer}  d-flex flex-column align-items-center text-center`}
+        >
           {/* title */}
-          <Image src={'/chat-header.jpg'} width={100} height={120} alt="chat header image" className={` mt-2`} />
-          <Image src={'/logo.jpg'} width={180} height={60} alt="chat header image" className={`${styles.welcomeImage} py-2`} />
+          <Image
+            src={'/chat-header.jpg'}
+            width={100}
+            height={120}
+            alt="chat header image"
+            className={` mt-2`}
+          />
+          <Image
+            src={'/logo.jpg'}
+            width={180}
+            height={60}
+            alt="chat header image"
+            className={`${styles.welcomeImage} py-2`}
+          />
           <p className={`mb-3 mt-2 ${styles.selectMethodtxt}`}>
             SELECT YOUR PREFERRED CHAT
           </p>
           {/* bot select card */}
-          <div className='d-flex flex-column justify-content-center align-items-center px-2 px-lg-4'>
-            <div className={`${styles.chatSelectBox}  col-12  p-3 d-flex flex-column align-items-center ${styles.boxColor1} me-lg-2 mb-2 mb-lg-0`}>
-              <Link href={'/chatbot'} >
-                <p className='text-dark text-uppercase mb-0'><strong>CONNECT WITH FINANCIAL ASSISTANT</strong></p>
+          <div className="d-flex flex-column justify-content-center align-items-center px-2 px-lg-4">
+            <div
+              className={`${styles.chatSelectBox}  col-12  p-3 d-flex flex-column align-items-center ${styles.boxColor1} me-lg-2 mb-2 mb-lg-0`}
+            >
+              <Link href={'/chatbot'}>
+                <p className="text-dark text-uppercase mb-0">
+                  <strong>CONNECT WITH AI ASSISTANT</strong>
+                </p>
               </Link>
             </div>
-            <div className={`${styles.chatSelectBox} col-12  p-3 d-flex flex-column align-items-center ${styles.boxColor2} me-lg-2 mb-2 mb-lg-0`}>
-              <Link href={'/liveAgent'} >
-                <p className='text-dark text-uppercase mb-0'><strong>CONNECT WITH A LIVE AGENT</strong></p>
+            {/* <div
+              className={`${styles.chatSelectBox} col-12  p-3 d-flex flex-column align-items-center ${styles.boxColor2} me-lg-2 mb-2 mb-lg-0`}
+            >
+              <Link href={'/liveAgent'}>
+                <p className="text-dark text-uppercase mb-0">
+                  <strong>CONNECT WITH A LIVE AGENT</strong>
+                </p>
               </Link>
-            </div>
-            
+            </div> */}
+
             {/* <div className={`${styles.chatSelectBox} col-12  p-3 d-flex flex-column align-items-center ${styles.boxColor3} me-lg-2 mb-2 mb-lg-0`}>
               <Link href={'/videobot'} >
                 <p className='text-dark text-uppercase mb-0'><strong>CONNECT VIA VIDEO</strong></p>
               </Link>
             </div>*/}
-            
-            <div className={`${styles.chatSelectBox} col-12  p-3 d-flex flex-column align-items-center ${styles.boxColor4} mb-2 mb-lg-0`}>
-              <Link href={'/audiobot'} >
-                <p className='text-dark text-uppercase mb-0'><strong>CONNECT VIA AUDIO</strong></p>
-              </Link>
-          </div> 
-          </div>
 
+            {/* <div
+              className={`${styles.chatSelectBox} col-12  p-3 d-flex flex-column align-items-center ${styles.boxColor4} mb-2 mb-lg-0`}
+            >
+              <Link href={'/audiobot'}>
+                <p className="text-dark text-uppercase mb-0">
+                  <strong>CONNECT VIA AUDIO</strong>
+                </p>
+              </Link>
+            </div> */}
+          </div>
         </div>
       </main>
     </>
